01fa95a87179085813f770c6c79c3c556e930137
Kevin Edin <kevin.edin@elev.ga.ntig.se>
2025-09-30 09:40:05 +0200

    lade till lista

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 import express from "express"
+import fs from "fs"
 const router = express.Router()
 
 router.get("/", (request, response) => {
-    response.render("index.njk")
-  })
+  response.render("index.njk")
+})
+
+router.get("/my_images", (request, response) => {
+  const {my_images} = JSON.parse(fs.readFileSync("./data/my_images.json"))
+  response.json(my_images)
+})
 
 export default router
